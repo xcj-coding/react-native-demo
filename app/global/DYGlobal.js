7e78e7af44010bfa4fb00b52bb83ae897870db82
xiongchangjiang <xcj@xcjIMAC.local>
2016-12-07 16:33:33 +0800

    丢段代码

--- app/global/DYGlobal.js
+++ app/global/DYGlobal.js
@@ -6,16 +6,53 @@ import {Dimensions} from 'react-native';
 
 const DYGlobal = {}
 
-DYGlobal.a = 1;
+DYGlobal.test = 'test';
 
+/**
+ * 获取屏幕高宽
+ */
 DYGlobal.window = {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
 };
 
-DYGlobal.fetch = {
-    
+/**
+ * fetch封装，只适用于移动端项目
+ * @param {[type]} obj 对象 url,type,dataType,data,success,error
+ */
+DYGlobal.getDataFromAPI = (obj) => {
+    let abj = {};
+
+    // 测试地址
+    abj.url = 'https://www.baidu.com/' + obj.url;
+
+    abj.type = obj.type || 'POST';
+    abj.dataType = obj.type || 'json';
+    abj.data = obj.data;
+    abj.success = obj.success || ((data)=>{console.log('成功回流: ' + data)});
+    abj.error = obj.error || ((data)=>{console.log('失败回流: ' + data)});
+    abj.fetchHeader = {};
+
+    if(abj.dataType === 'formData'){
+        // formData
+
+    }else if(abj.dataType === 'json'){
+        // json
+
+    }else{
+        // 图片
+
+    }
+
+    fetch(abj.url,fetchHeader).then(
+        abj.success
+    ).catch(
+        abj.error
+    )
+
 }
+
+
 
 
 module.exports = DYGlobal;
--- app/index.js
+++ app/index.js
@@ -11,18 +11,10 @@ import DYReducers from './reducers/rootReducer';
 import Root from './root'
 import DYGlobal from './global/DYGlobal'
 
-console.log(DYGlobal.a)
+console.log(DYGlobal.test)
 console.log(DYGlobal.window.width)
 console.log(DYGlobal.window.height)
 
-// const perflogger = store => next => action => {
-//   console.log( '%c Dispatching ', 'background: #222; color: #bada55', action );
-//   const start = performance.now();
-//   const result = next( action );
-//   const end = performance.now();
-//   console.log( `%c Action with type "${action.type}" took ${( end-start ).toFixed( 2 )} milliseconds.`, 'background: #bada55; color: #222' );
-//   return result;
-// }
 
 const logger = createLogger();
 
